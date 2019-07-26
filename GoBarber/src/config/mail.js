2d82e32d8f009cd2c2ac36baac7187e21330737c
Cesar <cesarcampos@gmail.com>
2019-07-25 23:57:27 -0300

    Implementacoes 25/07/2019

---
+++ GoBarber/src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '4f67bf72cff425',
+    pass: 'dbafdb157d5d5c',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
